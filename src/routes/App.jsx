19fef42796db1625ba19af9cc2c0d0700cb33595
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-14 12:27:43 -0600

    :sparkles: Add final content to app router

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,12 +3,47 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Redirect } from 'react-router'
 
 import { routes } from './routes'
+import Home from '../views/Home'
+import NuevoIngreso from '../views/NuevoIngreso'
+import Contacto from '../views/Contacto'
+import Layout from '../views/Layout'
+import { pageTitles } from '../utils/utils'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Redirect from='/' to='/inicio' />
-      <Switch></Switch>
+      <Redirect from='/' to={routes.HOME} />
+      <Switch>
+        <Route
+          exact
+          path={routes.HOME}
+          render={() => (
+            <Layout children={<Home />} activeItem={routes.HOME} pageTitle={pageTitles.HOME} />
+          )}
+        />
+        <Route
+          exact
+          path={routes.ADMISSIONS}
+          render={() => (
+            <Layout
+              children={<NuevoIngreso />}
+              activeItem={routes.ADMISSIONS}
+              pageTitle={pageTitles.ADMISSIONS}
+            />
+          )}
+        />
+        <Route
+          exact
+          path={routes.CONTACT}
+          render={() => (
+            <Layout
+              children={<Contacto />}
+              activeItem={routes.CONTACT}
+              pageTitle={pageTitles.CONTACT}
+            />
+          )}
+        />
+      </Switch>
     </BrowserRouter>
   )
 }
