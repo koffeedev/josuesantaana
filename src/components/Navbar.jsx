7c5a6aba92148b4c70d6442c32b1cb3a8a3839ca
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-14 14:21:12 -0600

    :hammer: Add navbar details and behavior

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
-import { Menu } from 'antd'
+import React, { useState } from 'react'
+import { Menu, Row, Col, Typography, Image } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, UserOutlined, MailOutlined } from '@ant-design/icons'
+
+import { routes } from '../routes/routes'
+import { schoolLogo } from '../utils/utils'
+import { styles } from '../utils/styles'
 
 const Navbar = ({ selectedKey }) => {
+  const [active, setActive] = useState(routes.HOME)
+
+  const handleActive = (key) => {
+    setActive(key)
+  }
+
   return (
-    <Menu theme='dark' mode='horizontal' defaultSelectedKeys={selectedKey}>
-      <Menu.Item key={1}>Inicio</Menu.Item>
-      <Menu.Item key={2}>Admisiones</Menu.Item>
-      <Menu.Item key={3}>Contacto</Menu.Item>
-    </Menu>
+    <Row>
+      <Col xl={1} lg={1} md={1} sm={1} xs={1} style={styles.centerVertical}>
+        <Link to={routes.HOME}>
+          <Image src={schoolLogo} height={55} preview={false} />
+        </Link>
+      </Col>
+      <Col xl={11} lg={11} md={11} sm={19} xs={19} style={styles.centerVertical}>
+        <Typography.Title level={2}>
+          <Link to={routes.HOME} style={styles.title}>
+            Colegio Josué
+          </Link>
+        </Typography.Title>
+      </Col>
+      <Col xl={12} lg={12} md={12} sm={4} xs={4}>
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          onClick={() => handleActive(selectedKey)}
+          defaultSelectedKeys={active}
+          style={styles.justifyEnd}
+        >
+          <Menu.Item key={routes.HOME} icon={<HomeOutlined />}>
+            <Link to={routes.HOME}>Inicio</Link>
+          </Menu.Item>
+          <Menu.Item key={routes.ADMISSIONS} icon={<UserOutlined />}>
+            <Link to={routes.ADMISSIONS}>Admisiones</Link>
+          </Menu.Item>
+          <Menu.Item key={routes.CONTACT} icon={<MailOutlined />}>
+            <Link to={routes.CONTACT}>Contacto</Link>
+          </Menu.Item>
+        </Menu>
+      </Col>
+    </Row>
   )
 }
 
