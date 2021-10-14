55175de767baf7f45e01d4b9f0c1ff7758db7af2
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-14 14:37:49 -0600

    :lipstick: Add styles in js

---
+++ src/utils/styles.js
@@ -0,0 +1,21 @@
+export const styles = {
+  title: {
+    color: '#ffffff',
+    marginBottom: '0',
+    lineHeight: '64px',
+  },
+  centerVertical: {
+    alignSelf: 'center',
+  },
+  justifyEnd: {
+    justifyContent: 'end',
+  },
+  header: {
+    position: 'fixed',
+    zIndex: 1,
+    width: '100%',
+  },
+  navCol: {
+    height: '64px',
+  },
+}
