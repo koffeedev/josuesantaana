26c7d677ea927e875dbaf7fb8b3f394a352c218e
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-14 12:27:57 -0600

    :hammer: Add utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,6 @@ export const pageTitles = {
   ADMISSIONS: 'Admisiones - Josué Santa Ana',
   CONTACT: 'Contacto - Josué Santa Ana',
 }
+
+export const collegeLogo =
+  'https://res.cloudinary.com/dqaav1s3t/image/upload/v1634233412/JosueSantaAna/logoColegio_xfvpft.png'
