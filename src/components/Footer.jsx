cde8c954acc51ba2f7895ce84e1cbc95e36c0eaa
Julio Canizalez <juliocanizalez@outlook.com>
2021-11-12 16:49:24 -0600

    :hammer: Update footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react'
-import { Layout, Row, Col } from 'antd'
+import { Layout, Row, Col, Divider } from 'antd'
 
 const Footer = () => {
   const date = useMemo(() => {
@@ -9,11 +9,18 @@ const Footer = () => {
   }, [])
 
   return (
-    <Row justify='center'>
-      <Col>
-        <Layout.Footer>Copyright &copy; Josu&eacute; Santa Ana {date}</Layout.Footer>
-      </Col>
-    </Row>
+    <>
+      <Row justify='center'>
+        <Col span={16}>
+          <Divider plain />
+        </Col>
+      </Row>
+      <Row justify='center'>
+        <Col>
+          <Layout.Footer>Copyright &copy; Josu&eacute; Santa Ana {date}</Layout.Footer>
+        </Col>
+      </Row>
+    </>
   )
 }
 
