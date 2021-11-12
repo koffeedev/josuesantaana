bcc2fd7e52b3608bab28ca350cfd50903f016c96
Julio Canizalez <juliocanizalez@outlook.com>
2021-11-12 16:48:42 -0600

    :mag: Add SEO

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -11,6 +11,15 @@ const Layout = ({ pageTitle, children, activeItem }) => (
   <HelmetProvider>
     <Helmet>
       <title>{pageTitle}</title>
+      <meta charset='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta
+        name='description'
+        content='Somos una Institución que responde al reto de la educación formativa integral en El Salvador, la cual debe incluir el cuerpo, el alma y el espíritu.'
+      />
+      <meta name='copyright' content='Josué Santa Ana' />
+      <meta name='robots' content='index, follow' />
+      <meta name='rating' content='general' />
     </Helmet>
     <Container>
       <Header>
