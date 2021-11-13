22dabb854a035a208742c377ce792035b4ab560a
Julio Canizalez <juliocanizalez@outlook.com>
2021-11-13 01:59:13 -0600

    :lipstick: Update styles

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -18,6 +18,8 @@ export const styles = {
   },
   section: {
     marginTop: '3rem',
+    marginLeft: '0',
+    marginRight: '0',
   },
   textSectionTitle: {
     textAlign: 'center',
@@ -29,5 +31,19 @@ export const styles = {
     width: '100%',
     gridTemplateColumns: '1fr',
     alignContent: 'center',
+  },
+  centerAndTextCenter: {
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  titleText: {
+    fontSize: '3.4rem',
+  },
+  descriptionText: {
+    fontSize: '1.3rem',
+    fontWeight: '400',
+  },
+  paddingZero: {
+    padding: '0',
   },
 }
